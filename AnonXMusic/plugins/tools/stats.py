7026cb4cce4c36bfaec9eb9c8f40b3c1c7340e6e
سورس الاعصار <118974844+BLAKAQ@users.noreply.github.com>
2023-11-09 05:46:20 +0400

    Update stats.py

--- AnonXMusic/plugins/tools/stats.py
+++ AnonXMusic/plugins/tools/stats.py
@@ -20,7 +20,7 @@ from AnonXMusic.utils.inline.stats import back_stats_buttons, stats_buttons
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["stats", "gstats", "أحصائياتي"]) & filters.group & ~BANNED_USERS)
+@app.on_message(command(["stats", "gstats", "أحصائياتي"]) & filters.group & ~BANNED_USERS)
 @language
 async def stats_global(client, message: Message, _):
     upl = stats_buttons(_, True if message.from_user.id in SUDOERS else False)
